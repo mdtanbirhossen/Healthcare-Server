5d233b6c9391b6ce13b817f8e092092215e89399
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2026-02-07 22:03:37 +0600

    feat: specialty create, delete and get all api

---
+++ src/app/module/specialty/specialty.controller.ts
@@ -0,0 +1,45 @@
+import { NextFunction, Request, RequestHandler, Response } from "express";
+import { SpecialtyService } from "./specialty.service";
+import { catchAsync } from "../../shared/catchAsync";
+import { sendResponse } from "../../shared/sendResponse";
+
+const createSpecialty = catchAsync(async (req: Request, res: Response) => {
+  const payload = req.body;
+  const result = await SpecialtyService.createSpecialty(payload);
+
+  sendResponse(res, {
+    httpStatusCode: 201,
+    success: true,
+    message: "Specialty created successfully",
+    data: result,
+  });
+});
+
+const getAllSpecialties = catchAsync(async (req: Request, res: Response) => {
+  const result = await SpecialtyService.getAllSpecialties();
+
+  sendResponse(res, {
+    success: true,
+    message: "Specialties fetched successfully",
+    data: result,
+    httpStatusCode: 200,
+  });
+});
+
+const deleteSpecialty = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await SpecialtyService.deleteSpecialty(id as string);
+
+  sendResponse(res, {
+    httpStatusCode: 200,
+    success: true,
+    message: "Specialty deleted successfully.",
+    data: result,
+  });
+});
+
+export const SpecialtyController = {
+  createSpecialty,
+  getAllSpecialties,
+  deleteSpecialty,
+};
