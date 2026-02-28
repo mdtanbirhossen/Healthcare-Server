eb9f635a24d3bafeb367a7ff1bfedcf55ee639be
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2026-02-28 12:44:53 +0600

    configured stripe

--- src/app/config/multer.config.ts
+++ src/app/config/multer.config.ts
@@ -1,6 +1,6 @@
+import multer from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 import { cloudinaryUpload } from "./cloudinary.config";
-import multer from "multer";
 
 const storage = new CloudinaryStorage({
     cloudinary: cloudinaryUpload,
@@ -8,27 +8,32 @@ const storage = new CloudinaryStorage({
         const originalName = file.originalname;
         const extension = originalName.split(".").pop()?.toLocaleLowerCase();
 
-        const filenameWithoutExtension = originalName
+        const fileNameWithoutExtension = originalName
             .split(".")
             .slice(0, -1)
             .join(".")
             .toLowerCase()
             .replace(/\s+/g, "-")
+            // eslint-disable-next-line no-useless-escape
             .replace(/[^a-z0-9\-]/g, "");
+
         const uniqueName =
-            Math.random().toString(36).substring(2) +
-            "-" +
-            Date.now() +
-            "-" +
-            filenameWithoutExtension;
+            Math.random().toString(36).substring(2)+
+            "-"+
+            Date.now()+
+            "-"+
+            fileNameWithoutExtension;
 
         const folder = extension === "pdf" ? "pdfs" : "images";
-        return {
-            folder: `health-care/${folder}`,
-            public_id: uniqueName,
-            resource_type: "auto",
-        };
-    },
-});
 
-export const multerUpload = multer({ storage });
+
+        return {
+            folder : `ph-healthcare/${folder}`,
+            public_id: uniqueName,
+            resource_type : "auto"
+        }
+    }
+
+})
+
+export const multerUpload = multer({storage})
